06e96cbe82ec99ee58b9940cf6e6e2995a1b045e
Michael James <thelivingpc@gmail.com>
2017-02-26 02:10:05 -0500

    reorganize

---
+++ main.js
@@ -0,0 +1,101 @@
+
+//////////////////////
+// variables
+//////////////////////
+
+var phantomFile = 'scraper-phantom.js';
+
+/*
+select one of the following animals:
+* TigerPopulation
+* SouthChinaTiger
+* AmurTiger
+* BengalTiger
+* AmurLeopard
+* MountainGorilla
+* Sumatranorangutan
+* GiantPanda
+* AsianElephant
+* SumatranElephant
+* BlackRhino
+* JavanRhino
+* HawksbillTurtlenestingfemales
+* LeatherbackTurtlenestingfemales
+* BlueWhale
+* Baiji
+* Vaquita
+* YangtzeFinlessPorpoise
+*/
+var animal = "GiantPanda"
+
+//////////////////////
+
+var fs = require('fs');
+var path = require('path')
+var childProcess = require('child_process')
+var phantomjs = require('phantomjs-prebuilt')
+var binPath = phantomjs.path
+var childArgs = [path.join(__dirname, phantomFile)]
+
+// scrape webpage
+childProcess.execFile(binPath, childArgs, function(err, newStr, stderr) {
+  if (stderr) {
+  	console.log(stderr);      // Always empty
+ 	}
+  writeFiles(newStr);
+})
+
+// save data
+function writeFiles(newStr) {
+	// rename old JSON file
+  fs.readFile('json-newest.js', 'utf8', (err, prevStr) => {
+  	if (!err) {
+	  	fs.writeFile('json-previous.js', prevStr, 'utf8', function(err) {
+			  if(err) {
+			  	return console.log(err);
+				}
+			  
+			  console.log("old file renamed!");
+
+			  // save JSON to file
+			  fs.writeFile("json-newest.js", newStr, 'utf8', function(err) {
+				  if(err) {
+				  	return console.log(err);
+					}
+				  
+				  console.log("new file saved!");
+				});
+
+			  processJSON(newStr, prevStr);
+			});
+		}
+		else {
+			console.log("history not find.")
+		}
+	});
+}
+
+// parse and act on data
+function processJSON(newStr, prevStr) {
+	var newJSON = JSON.parse(newStr);
+	console.log(newJSON);
+  console.log("myAnimal: " + newJSON[animal]);
+	var prevJSON = JSON.parse(prevStr);
+
+	// if an animal has died
+	if (newJSON[animal] > prevJSON[animal]) {
+		startRitual();
+	}
+	else {
+		console.log("still alive!")
+	}
+}
+
+// hardware ritual
+function startRitual() {
+	console.log("** glitter **");
+}
+
+
+
+
---
+++ scraper-phantom.js
@@ -0,0 +1,45 @@
+// Read animal extinction stats using PhantomJS, jQuery, and "includeJs"
+
+"use strict";
+var system = require('system');
+var page = require('webpage').create();
+
+page.onConsoleMessage = function(msg) {
+    console.log(msg);
+};
+
+// page to scrape
+var site = "http://www.poodwaddle.com/worldclock/env5/";
+
+page.open(site, function(status) {
+    if (status === "success") {
+        page.includeJs("http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js", function() {
+            page.evaluate(function() {
+                // scrape!
+                jsonObj = {};
+
+                // animal element
+                var selector = ".WCs5";
+
+                $(selector).each(function() {
+                    // animal name (remove spc chars and whitespace)
+                    var name = $(this).find(".WCsl").text().replace(/[^\w\s]/gi, '').replace(/\s/g,'');
+                    // animal count (remove spc chars such as comma)
+                    var count = $(this).find(".WCsv").text().replace(/[^\w\s]/gi, '');
+
+                    // store data, with count as number
+                    jsonObj[name] = eval(count);
+                });
+
+                // data to JSON, back to Node
+                console.log(JSON.stringify(jsonObj));
+            });
+            phantom.exit(0);
+        });
+    } else {
+        phantom.exit(1);
+    }
+});
+
+
+
