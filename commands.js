fa873aa6788b7a8aa47a42937d3a901c932baea4
Michael James <thelivingpc@gmail.com>
2017-02-28 23:36:12 -0500

    notes

---
+++ commands.js
@@ -0,0 +1,14 @@
+
+// paste in the following to ssh into the Pi
+ssh pi@MJ-RASPBPI.WV.CC.CMU.EDU
+
+// type in the password when prompted
+qscTg145
+
+// paste in the following to set the working directory
+cd Projects/extinction-alert/alert
+
+// run the alert.js program
+node alert.js
+
+// to quit you may have to do ctrl + \
